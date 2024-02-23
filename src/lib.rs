441ab021053d2c11c3ecb523a5fb8d32673da5e6
Eric Holk <eric@theincredibleholk.org>
2024-02-22 22:28:23 -0800

    Support ranges and multiple entries

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,50 @@
 use std::collections::BTreeSet;
 
-use nom::error::VerboseError;
+use nom::{branch::alt, bytes::complete::tag, multi::separated_list1};
 use thiserror::Error;
 
 pub fn parse_number_set(input: &str) -> Result<Vec<usize>, ParseError<'_>> {
     let mut set = BTreeSet::new();
 
-    let (_input, number) =
-        nom::character::complete::u64::<_, VerboseError<_>>(input).map_err(ParseError::NomError)?;
-
-    set.insert(number as usize);
+    let (_, entries) = separated_list1(tag(","), alt((single_range, single_num)))(input)
+        .map_err(ParseError::NomError)?;
+    for e in entries {
+        match e {
+            Entry::Number(n) => {
+                set.insert(n);
+            }
+            Entry::Range(start, end) => {
+                for n in start..=end {
+                    set.insert(n);
+                }
+            }
+        }
+    }
 
     Ok(set.into_iter().collect())
+}
+
+enum Entry {
+    Number(usize),
+    Range(usize, usize),
+}
+
+fn single_num(input: &str) -> nom::IResult<&str, Entry> {
+    let (input, number) = nom::character::complete::u64(input)?;
+    Ok((input, Entry::Number(number as usize)))
+}
+
+fn single_range(input: &str) -> nom::IResult<&str, Entry> {
+    let (input, start) = nom::character::complete::u64(input)?;
+    let (input, _) = alt((tag("-"), tag("..")))(input)?;
+    let (input, end) = nom::character::complete::u64(input)?;
+    Ok((input, Entry::Range(start as usize, end as usize)))
 }
 
 #[derive(Debug, Error)]
 pub enum ParseError<'a> {
     #[error(transparent)]
-    NomError(nom::Err<VerboseError<&'a str>>),
+    NomError(nom::Err<nom::error::Error<&'a str>>),
 }
 
 #[cfg(test)]
@@ -28,6 +55,20 @@ mod tests {
     fn one_number() -> anyhow::Result<()> {
         let result = parse_number_set("42")?;
         assert_eq!(result, vec![42]);
+        Ok(())
+    }
+
+    #[test]
+    fn one_range() -> anyhow::Result<()> {
+        let result = parse_number_set("1..3")?;
+        assert_eq!(result, vec![1, 2, 3]);
+        Ok(())
+    }
+
+    #[test]
+    fn numbers_and_ranges() -> anyhow::Result<()> {
+        let result = parse_number_set("1,3,5..7,9,12..14")?;
+        assert_eq!(result, vec![1, 3, 5, 6, 7, 9, 12, 13, 14]);
         Ok(())
     }
 }
